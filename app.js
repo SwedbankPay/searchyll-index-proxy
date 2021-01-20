d02ea96be7e864a46840a6ae40dccbeb6ce24cd6
Daniel Granerud <daniel.granerud@payex.com>
2021-01-20 14:42:09 +0100

    Use auth header for elastic

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 
 const apiKey = process.env.apiKey || "super-secret-key";
 const elasticUrl = process.env.elasticHost || 'localhost:9200'
+const elasticAuth = process.env.elasticAuth || 'none'
 
 app.use('/', proxy(elasticUrl, {
   limit: '100mb',
@@ -41,7 +42,7 @@ app.use('/', proxy(elasticUrl, {
   },
   userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
     // recieves an Object of headers, returns an Object of headers.
-    headers['Authorization'] = null;
+    headers['Authorization'] = elasticHost;
     return headers;
   }
 }));
